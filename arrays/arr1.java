bc5f4e934aae075d6f658fc27085a609a26a10c5
RIYA KUMARI <riyakumarisharma9@gmail.com>
2025-09-25 21:35:58 +0530

    Add files via upload

---
+++ arrays/arr1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class arr1 {
+    public static void main(String args[])
+    // create array and print all the elements
+    {
+        int arr[] = { 1, 4, 5, 7, 4, 8, 4 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
